585dc79b135c64b9598e95af88fd74745be637e6
ViktoriaSinel <86431229+ViktoriaSinel@users.noreply.github.com>
2021-07-16 13:30:37 -0700

    commit

--- Bitcoin.js
+++ Bitcoin.js
@@ -5,47 +5,6 @@ title('Bitcoin');
 
 const DATE_FORMAT = 'YYYY-MM-DD';
 
-intent(
-    '(what was|what were|tell me|how much was|do you know|) (the|) Bitcoin (price_) (in the|over the|) past $(PERIOD week|month|year)',
-    '(what is|what was|what were|do you know) the past $(PERIOD week|month|year) Bitcoin (price_)',
-    'what (was|were) the Bitcoin (price_)',
-    async p =>  {
-        const period = p.PERIOD ? p.PERIOD.value : 'week';
-        const start = getStartDate(period, p.timeZone);
-        const end = api.moment().format(DATE_FORMAT);
-
-        const url = `https://api.coindesk.com/v1/bpi/historical/close.json?currency=USD&start=${start}&end=${end}`;
-        const data = await getData(url);
-
-        if (!data) {
-            return sendErrorMessage(p);
-        }
-
-        const values = [];
-        const dates = [];
-
-        Object.keys(data.bpi).forEach(date => {
-            const value = Math.trunc(data.bpi[date]);
-            dates.push(date);
-            values.push(value);
-        });
-
-        const max = Math.max(...values);
-        const min = Math.min(...values);
-
-        p.play({
-            embeddedPage: true,
-            page: 'bitcoin_example.html',
-            command: 'newChart',
-            values,
-            dates,
-        });
-        p.play(
-            `(For|Over) the past ${period} the lowest Bitcoin price was ${min} dollars and the highest Bitcoin price was ${max} dollars`,
-            `The past ${period}'s lowest Bitcoin price was ${min} dollars and the highest Bitcoin price was ${max} dollars`,
-        );
-    }
-);
 
 intent(
     'how much is Bitcoin',
